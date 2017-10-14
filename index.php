b85773a693929e057c7e7fdc52e784b8fcfea0c3
Luca <lvkan10@qq.com>
2017-10-14 17:27:29 +0800

    Luca
    程序更新

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if (strpos($ua, 'Alipay')) {
 } elseif (strpos($ua, 'QQ/')) {
     //微信
     $config = array(
-        "img"  => "qrcode/qq.gif",
+        "url"  => "http://vac.qq.com/wallet/qrcode.htm?m=tenpay&a=1&u=839488083&ac=7B411D7D0588EBECADFAF6E63B7619DE1B6906DDFD7C1B9919FF8442F8A7C5C0&n=%E5%B0%8F%E5%B0%8F%E9%85%A5&f=wallet",
         "text" => "长按二维码付款",
         "col"  => "#0099de",
         "new"  => 1,
