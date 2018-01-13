a5d5212bbbc3a4ef8b76e99493d49835cb503e1c
LuckyMoke <lvkan10@qq.com>
2018-01-13 20:05:08 +0800

    Luca
    修复一些Bug，修改配置方式

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//支付宝链接
+$alipay_url = 'https://qr.alipay.com/tsx099685ffdf96nzasre20';
+//微信支付链接
+$wechat_url = 'https://payapp.weixin.qq.com/qr/AQGXFj0KWP93Es0y3w5rYzdY?t=CwAK#wechat_pay';
+//qq支付链接
+$qq_url     = 'http://vac.qq.com/wallet/qrcode.htm?m=tenpay&a=1&u=839488083&ac=7B411D7D0588EBECADFAF6E63B7619DE1B6906DDFD7C1B9919FF8442F8A7C5C0&n=%E5%B0%8F%E5%B0%8F%E9%85%A5&f=wallet';
+//京东金融支付链接
+$jd_url     = 'https://sq.jd.com/pay/Zx1fZ4';
